238d6eedf01c82473a3116ae1b357946557580db
lmshasta <lindanismoyo@gmail.com>
2020-05-17 11:09:44 +0200

    day 2

--- day_1.R
+++ day_1.R
@@ -41,16 +41,21 @@
 # LIBRARIES -----------------------------------------------------------------------------------------------------------
 
 library(rpart)               # Decision Tree models
+# install.packages("rattle")
 library(rattle)              # Visualising Decision Tree models (can be a problem to install on Windows)
 library(rpart.plot)          # Visualising Decision Tree models
+# install.packages("Metrics")
 library(Metrics)             # Evaluating model performance
 library(MASS)                # Stepwise Algorithm
+# install.packages("caret")
 library(caret)               # Swiss Army Knife
 
 library(readr)               # Reading CSV files
 library(dplyr)               # General data wrangling
 library(forcats)             # Working with factors
+# install.packages("janitor")
 library(janitor)             # Cleaning column names
+# install.packages("naniar")
 library(naniar)              # Missing data
 library(ggplot2)             # Wicked plots
 library(lubridate)           # Handling date/time data
@@ -84,6 +89,8 @@ str(insurance)
 # Look at a "spreadsheet" view of data.
 #
 View(insurance)
+
+summary(insurance)
 #
 # What do all of those fields mean?
 
@@ -351,6 +358,261 @@ set.seed(13)
 #
 index <- sample(c(TRUE, FALSE), nrow(insurance), replace = TRUE, prob = c(0.8, 0.2))
 
+index
+
+train <- insurance[index,]
+test <- insurance[!index,]
+
+# Check that the proportions are correct (should be roughly 4x as many records in training data).
+#
+nrow(train)
+nrow(test)
+
+# What about the proportions of the target variable?
+#
+prop.table(table(train$car_insurance))
+prop.table(table(test$car_insurance))
+#
+# These look fairly close. Good enough for the moment.
+
+# MODEL #1 ------------------------------------------------------------------------------------------------------------
+
+# A Decision Tree model is a great place to start. It should produce reasonable result 
+# and it also gives a model which
+# is simple to interpret.
+
+model_rpart <- rpart(car_insurance ~ ., data = train)
+#
+# This is a "kitchen sink" model: we're throwing in all of the features.
+#
+# Fortunately a Decision Tree works rather well for this because it'll perform
+# implicit feature selection, retaining
+# only those features which do actually contribute.
+
+# Take a quick look at the model.
+#
+model_rpart
+#
+# What's the most important feature?
+
+# Plot the tree.
+#
+fancyRpartPlot(model_rpart)
+rpart.plot(model_rpart, cex = 0.75)
+#
+# Interpretations:
+#
+# - The longer the call, the more likely the sale.
+# - Long calls less effective for elderly. Simply enjoy the chat?
+
+# MODEL ASSESSMENT ----------------------------------------------------------------------------------------------------
+
+# Make predictions on the testing data.
+#
+test_predictions <- predict(model_rpart, test)
+head(test_predictions)
+#
+# These are the predicted probabilities of the outcome classes.
+#
+# Can we get a class prediction?
+#
+?predict.rpart
+#
+# We need to specify the 'type' parameter.
+
+# Specify that we want to predict classes.
+#
+test_predictions <- predict(model_rpart, test, type = "class")
+head(test_predictions)
+#
+# Bingo!
+
+# So how well does the model perform? Need to compare the known to the predicted classes.
+#
+head(test$car_insurance)
+#
+# Looks reasonable. But let's be more rigorous!
+
+# Compare predictions to known values.
+#
+test_predictions == test$car_insurance
+
+# What proportion of these are correct?
+#
+mean(test_predictions == test$car_insurance)
+
+# There's a function for this!
+#
+accuracy(test$car_insurance, test_predictions)
+#
+# This is the proportion of predictions that are correct.
+#
+# There's a problem with this though: if our model is really good at predicting the negative class then it will still
+# have good accuracy.
+
+# MODEL #2 ------------------------------------------------------------------------------------------------------------
+
+# Unfortunately our model appears to be better than it actually is.
+#
+# Why?
+#
+# Because it's reliant on a feature that we could not have prior to contacting the potential client: call duration!
+
+# 🚨 Exercise
+#
+# 1. Remove the columns relating to the call.
+# 2. Repeat the train/test split.
+# 3. Build a Decision Tree model.
+# 4. Plot the tree. What's the most important feature?
+# 4. Make predictions on the testing set.
+# 5. Calculate the accuracy.
+
+insurance <- insurance %>% select(-id)
+
+insurance1 <- insurance
+names(insurance1)
+
+insurance1 <- insurance1 %>% select(-call_duration)
+names(insurance1)
+# Generally you want to have around 80:20 split.
+#
+index1 <- sample(c(TRUE, FALSE), nrow(insurance1), replace = TRUE, prob = c(0.8, 0.2))
+
+index1
+
+train1 <- insurance1[index1,]
+test1 <- insurance1[!index1,]
+
+# Check that the proportions are correct (should be roughly 4x as many records in training data).
+#
+nrow(train1)
+nrow(test1)
+
+
+# What about the proportions of the target variable?
+#
+prop.table(table(train1$car_insurance))
+prop.table(table(test1$car_insurance))
+#
+# These look fairly close. Good enough for the moment.
+
+# MODEL #1 ------------------------------------------------------------------------------------------------------------
+
+# A Decision Tree model is a great place to start. It should produce reasonable result 
+# and it also gives a model which
+# is simple to interpret.
+
+model_rpart1 <- rpart(car_insurance ~ ., data = train1)
+#
+# This is a "kitchen sink" model: we're throwing in all of the features.
+#
+# Fortunately a Decision Tree works rather well for this because it'll perform
+# implicit feature selection, retaining
+# only those features which do actually contribute.
+
+# Take a quick look at the model.
+#
+model_rpart1
+#
+# What's the most important feature?
+
+# Plot the tree.
+#
+fancyRpartPlot(model_rpart1)
+rpart.plot(model_rpart1, cex = 0.75)
+#
+# Interpretations:
+#
+# - The longer the call, the more likely the sale.
+# - Long calls less effective for elderly. Simply enjoy the chat?
+
+# MODEL ASSESSMENT ----------------------------------------------------------------------------------------------------
+
+# Make predictions on the testing data.
+#
+test_predictions1 <- predict(model_rpart1, test1)
+head(test_predictions1)
+#
+# These are the predicted probabilities of the outcome classes.
+#
+# Can we get a class prediction?
+#
+?predict.rpart
+#
+# We need to specify the 'type' parameter.
+
+# Specify that we want to predict classes.
+#
+test_predictions1 <- predict(model_rpart1, test1, type = "class")
+head(test_predictions1)
+#
+# Bingo!
+
+# So how well does the model perform? Need to compare the known to the predicted classes.
+#
+head(test1$car_insurance)
+#
+# Looks reasonable. But let's be more rigorous!
+
+# Compare predictions to known values.
+#
+test_predictions1 == test1$car_insurance
+
+# What proportion of these are correct?
+#
+mean(test_predictions1 == test1$car_insurance)
+
+# There's a function for this!
+#
+accuracy(test$car_insurance, test_predictions)
+#0.7385943
+
+
+
+
+# DAY OF WEEK ---------------------------------------------------------------------------------------------------------
+
+# 🚨 Exercise (BONUS)
+#
+# We have the month and day of last contact. Is it possible to guess the year?
+#
+# It's possible that not all of the contacts happened in the same year, but you should be able to get an idea of a
+# likely year.
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+# Astrid Radermacher11:03 AM
+# A Question we (me, Farrah and Naadirah) had from yesterday was how
+# to interpret the decision tree
+
+# MODEL GAME PLAN -----------------------------------------------------------------------------------------------------
+
+# 1. Decision Tree
+# 2. Decision Tree (removing snooped feature)
+# 3. Logistic Regression
+# 4. Logistic Regression + feature selection
+# 5. Logistic Regression using {caret}
+# 6. Decision Tree using {caret} (introducing hyper-parameter optimisation)
+# 7. XGBoost using {caret} (final model)
+
+# TRAIN/TEST SPLIT ----------------------------------------------------------------------------------------------------
+
+# In order to assess how well our model performs we need to split it into two components:
+#
+# - training and
+# - testing.
+#
+# There are a number of ways to do this split.
+
+# Set the RNG seed so that everybody gets the same train/test split.
+#
+set.seed(13)
+
+# Generally you want to have around 80:20 split.
+#
+index <- sample(c(TRUE, FALSE), nrow(insurance), replace = TRUE, prob = c(0.8, 0.2))
+
 train <- insurance[index,]
 test <- insurance[!index,]
 
@@ -457,6 +719,28 @@ accuracy(test$car_insurance, test_predictions)
 # 5. Calculate the accuracy.
 
 # === -> YOUR CODE ===
+# Let's create a fmore realisticdataset.
+#
+# We need to remove features that relate to the call.
+#
+# Why?
+#
+# Because we don't know the time or duration of the call in advance. These are not characteristics of the prospective
+# customer. So to use these data we are effectively "snooping" the results.
+#
+insurance <- insurance %>% select(-starts_with("call_"))
+#
+train <- insurance[index,]
+test <- insurance[!index,]
+
+model_rpart <- rpart(car_insurance ~ ., data = train)
+
+test_predictions <- predict(model_rpart, test, type = "class")
+
+accuracy(test$car_insurance, test_predictions)
+
+fancyRpartPlot(model_rpart)
+rpart.plot(model_rpart, cex = 0.75)
 # === <- YOUR CODE ===
 
 # DAY OF WEEK ---------------------------------------------------------------------------------------------------------
@@ -469,4 +753,272 @@ accuracy(test$car_insurance, test_predictions)
 # likely year.
 
 # === -> YOUR CODE ===
+# Strategy: Guess a year and then find the day of week corresponding to the specified month and day. Find a year which
+# minimises the number of calls on Saturday and Sunday.
+
+test_year <- function(year) {
+  insurance %>%
+    mutate(
+      last_contact_date = as.Date(sprintf("%d-%s-%d", year, last_contact_month, last_contact_day), format = "%Y-%b-%d"),
+      last_contact_weekday = wday(last_contact_date, label = TRUE)
+    ) %>%
+    select(starts_with("last_contact")) %>%
+    count(last_contact_weekday)
+}
+
+test_year(2017)
+test_year(2016)
+test_year(2015) # This looks the most likely.
+test_year(2014)
+
 # === <- YOUR CODE ===
+
+# MODEL #3: LOGISTIC REGRESSION ---------------------------------------------------------------------------------------
+
+# 📌 Logistic Function
+#
+tibble(
+  x = seq(-15, 15, 0.25),
+  y = plogis(x)
+) %>% ggplot(aes(x, y)) + geom_line()
+#
+# A Logistic Regression model uses a logistic "link function" to map the real numbers onto the interval [0, 1].
+
+model_glm <- glm(car_insurance ~ ., data = train, family = binomial)
+
+summary(model_glm)
+#
+# All of the features get coefficients, many of which are not statistically significant.
+
+# Let's take a look at how this model performs.
+#
+test_predictions <- predict(model_glm, test, type = "response")
+head(test_predictions)
+#
+# These are effectively probabilities. We need to apply a threshold to convert them to classes.
+#
+test_predictions <- ifelse(test_predictions > 0.5, 1, 0)
+
+accuracy(test$car_insurance, test_predictions)
+#
+# The performance of the Logistic Regression model is similar to that of the Decision Tree.
+
+# MODEL #4: FEATURE SELECTION -----------------------------------------------------------------------------------------
+
+# It looks like the 'days_passed' feature is not contributing to the model, so let's remove it and see what happens.
+#
+model_glm_trimmed <- glm(car_insurance ~ . - days_passed, data = train, family = binomial)
+
+# What is the test accuracy for our streamlined model?
+#
+test_predictions <- ifelse(predict(model_glm_trimmed, test, type = "response") > 0.5, 1, 0)
+accuracy(test$car_insurance, test_predictions)
+#
+# Looks like the model is no worse than before. And it's simpler (or more "parsimonius").
+
+# We could narrow down the selection of coefficients manually.
+#
+# This would be an arduous process of trial and error.
+#
+# We can also automate the process. There are a variety of approaches to this. Here are two options:
+#
+# - stepwise algorithm [*]
+# - penalised regression (the Lasso or Ridge regression techniques, which are actually better!).
+#
+model_glm <- stepAIC(model_glm)
+
+# What terms are there in the new model?
+#
+model_glm$formula
+#
+# We've dropped these predictors:
+#
+# - job
+# - marital
+# - default
+# - balance
+# - last_contact_day and
+# - days_passed.
+#
+# So the new model is considerably more parsimonious.
+
+# What effect has this had on model performance?
+#
+test_predictions <- predict(model_glm, test, type = "response")
+test_predictions <- ifelse(test_predictions > 0.5, 1, 0)
+#
+accuracy(test$car_insurance, test_predictions)
+#
+# It's not quite as good as the model with all of the terms, but the difference is really very small (and could
+# change with a different train/test split).
+
+# MODEL #5: CARET / LOGISTIC REGRESSION -------------------------------------------------------------------------------
+
+# Let's take the model up a notch and use {caret}.
+
+# First we need to make a small change to the target variable because caret expects the positive class to be the first
+# level.
+#
+insurance$car_insurance = relevel(insurance$car_insurance, "1")
+levels(insurance$car_insurance) <- c("yes", "no")
+#
+train <- insurance[index,]
+test <- insurance[!index,]
+
+# 📌 Testing, Validation & Cross-Validation
+#
+# https://raw.githubusercontent.com/datawookie/useful-images/master/data-train-test.svg
+# https://raw.githubusercontent.com/datawookie/useful-images/master/data-train-test-validate.svg
+# https://raw.githubusercontent.com/datawookie/useful-images/master/cross-validation.svg
+
+# We'll start off with a "simple" model, Logistic Regression, which doesn't have any hyper-parameters.
+
+# In caret models are created with train().
+#
+# What models are possible?
+#
+names(getModelInfo())
+
+model_glm <- train(car_insurance ~ ., data = train, method = "glm")
+
+model_glm
+#
+# Get more details.
+#
+model_glm$results
+#
+# We get:
+#
+# - the average of accuracy
+# - the standard deviation of accuracy (gives us an indication of uncertainty in accuracy estimate)
+#
+# across 25 bootstrap resamples of the data.
+#
+# Using the standar deviation we can construct a confidence interval for the accuracy (two standard deviations gives
+# a 95% confidencen interval).
+
+# Note the "parameter" column: we'll be coming back to that shortly.
+#
+# The accuracy estimate is far more robust because it's been calculated with boostrapping rather than a single split.
+
+# MODEL #6: CARET / DECISION TREE -------------------------------------------------------------------------------------
+
+# Next we are going to do the following:
+#
+# - change from Logistic Regression to Decision Tree by simply modifying the 'method' argument
+# - be more specific about validation, using 10-fold cross-validation.
+
+TRAINCONTROL = trainControl(
+  method = "cv",
+  number = 10,
+  verboseIter = TRUE
+)
+
+model_rpart <- train(car_insurance ~ ., data = train, method = "rpart", trControl = TRAINCONTROL)
+
+model_rpart$results
+#
+# Aha!
+#
+# There's now a table for validation results for a few values of the 'cp' (complexity parameter).
+#
+# The train() function cleverly selects the "best" model on the basis of accuracy.
+
+# Generate predictions on the testing data.
+#
+test_predictions <- predict(model_rpart, test)
+
+confusionMatrix(test_predictions, test$car_insurance)
+#
+# Now we have access to a whole suite of metrics (in addition to the accuracy):
+#
+# sensitivity - what proportion of the positive values are correctly predicted [*]
+# specificity - what proportion of the negative values are correctly predicted
+#
+# positive predictive value - what proportion of the positive predictions are correct [*]
+# negative predictive value - what proportion of the negative predictions are correct
+#
+# What do we observe?
+#
+# - the test accuracy is high but
+# - the test sensitivity is low (which means that we are not very good at picking good prospects) but
+# - the test PPV is high (which means that the ones we do pick are likely to be good!).
+#
+# So the model is useful but it could probably still be improved a lot.
+
+# Can we use the predictions to rate prospective clients?
+#
+# Get the predicted probability of a successful call.
+#
+test_probabilities <- predict(model_rpart, test, type = "prob")$yes
+#
+# Add these probabilities as a column in the test data.
+#
+test %>%
+  mutate(rating = test_probabilities) %>%
+  arrange(desc(rating))
+
+# What are the most important predictors?
+#
+varImp(model_rpart)
+
+# MODEL #7: CARET / XGBOOST -------------------------------------------------------------------------------------------
+
+# Now let's try out a "black box" model: XGBoost. The is a popular model on Kaggle which uses gradient boosting.
+
+HYPER_PARAMETER_GRID <- expand.grid(
+  gamma = 0,
+  min_child_weight = 1,
+  nrounds = 100,
+  max_depth = 5,
+  eta = c(0.40, 0.50),
+  colsample_bytree = c(0.7, 0.8),
+  subsample = 0.8
+)
+
+TRAINCONTROL = trainControl(
+  method = "cv",
+  # Set this to 5 so that model buils quickly, but probably better to use 10 (for superstitious reasons alone!).
+  number = 5,
+  classProbs = TRUE,
+  summaryFunction = twoClassSummary,
+  verboseIter = TRUE
+)
+
+model_xgboost <- train(
+  car_insurance ~ .,
+  data = train,
+  method = "xgbTree",
+  # Optimise the model for sensitivity.
+  metric = "Sens",
+  trControl = TRAINCONTROL,
+  tuneGrid = HYPER_PARAMETER_GRID
+)
+
+# Look at results of grid-search on hyper-parameters.
+#
+model_xgboost$results %>% arrange(desc(Sens)) %>% head()
+
+test_predictions <- predict(model_xgboost, test)
+
+confusionMatrix(test_predictions, test$car_insurance)
+#
+# Finally a model that's better than random guessing! Compare the model sensitivity to the 40% chance of guessing
+# correctly.
+
+# Rate predictions in descending order.
+#
+test %>%
+  mutate(rating = predict(model_xgboost, test, type = "prob")$yes) %>%
+  arrange(desc(rating))
+
+varImp(model_xgboost)
+
+# ROADMAP -------------------------------------------------------------------------------------------------------------
+
+# Things we could do to improve our model:
+#
+# - Balance the proportion of the target variable.
+# - Explore other models.
+# - More hyper-parameter tuning.
+# - Engineer new features.
